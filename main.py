6b1b8ce9efc98569e34001c95ccce357f74d8b52
supermentos <supermentos12@gmail.com>
2025-09-05 09:36:17 +0300

    v0.3 Just a bug fix

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ This is converter
 def convertCtoF():
     c = float(input("Ievadi C:"))
     F = (C * 9/5) + 32
-    print F
+    print(F)
 
 #This converts kilometers to miles
 def convertKilometerToMiles():
     kms = float(input("Ievadi ātttālumu:"))
-    miles = round(kms/1.6)
-    print ("Rezultāts ir: {miles}")
+    if kms > 0:
+        miles = round(kms/1.6)
+        print("Rezultāts ir:", miles)
+    else:
+        print("Vērtība nederīga")
+
+convertKilometerToMiles()
